a06f5991c3f35c6e74fbe0e01aad81108121fb00
Diego Ferreira <diegoof@gmail.com>
2021-04-21 15:22:07 -0400

    eliminated the "public partial class Program" out of classes

--- RomanToInteger/Roman.cs
+++ RomanToInteger/Roman.cs
@@ -1,23 +1,23 @@
 ﻿namespace RomanToInteger
 {
-    public partial class Program
+    /// <summary>
+    /// Constant values for the roman number
+    /// </summary>
+    public class Roman
     {
-        public class Roman
+        public char Value { get; private set; }
+
+        public static readonly Roman I = new('I');
+        public static readonly Roman V = new('V');
+        public static readonly Roman X = new('X');
+        public static readonly Roman L = new('L');
+        public static readonly Roman C = new('C');
+        public static readonly Roman D = new('D');
+        public static readonly Roman M = new('M');
+
+        private Roman(char value)
         {
-            public char Value { get; private set; }
-
-            public static readonly Roman I = new ('I');
-            public static readonly Roman V = new ('V');
-            public static readonly Roman X = new ('X');
-            public static readonly Roman L = new ('L');
-            public static readonly Roman C = new ('C');
-            public static readonly Roman D = new ('D');
-            public static readonly Roman M = new ('M');
-
-            protected Roman(char value)
-            {
-                this.Value = value;
-            }
+            this.Value = value;
         }
     }
 }
--- RomanToInteger/RomanIntegerMap.cs
+++ RomanToInteger/RomanIntegerMap.cs
@@ -2,28 +2,25 @@
 
 namespace RomanToInteger
 {
-    public partial class Program
+    /// <summary>
+    /// The map between Roman and Integer values
+    /// </summary>
+    public static class RomanIntegerMap
     {
-        /// <summary>
-        /// The map between Roman and Integer values
-        /// </summary>
-        public static class RomanIntegerMap
-        {            
-            private static readonly Dictionary<char, int> _map = new()
-            {
-                { Roman.I.Value, 1},
-                { Roman.V.Value, 5},
-                { Roman.X.Value, 10},
-                { Roman.L.Value, 50},
-                { Roman.C.Value, 100},
-                { Roman.D.Value, 500},
-                { Roman.M.Value, 1000}                
-            };
+        private static readonly Dictionary<char, int> _map = new()
+        {
+            { Roman.I.Value, 1 },
+            { Roman.V.Value, 5 },
+            { Roman.X.Value, 10 },
+            { Roman.L.Value, 50 },
+            { Roman.C.Value, 100 },
+            { Roman.D.Value, 500 },
+            { Roman.M.Value, 1000 }
+        };
 
-            public static int GetValueByKey(char Key)
-            {
-                return _map.GetValueOrDefault(Key);
-            }
+        public static int GetValueByKey(char Key)
+        {
+            return _map.GetValueOrDefault(Key);
         }
     }
 }
